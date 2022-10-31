2eeac1d1388a2bb906e5e521e872eb7dfb92f74c
Jong Wook Kang <50934118+timbergrizz@users.noreply.github.com>
2022-10-31 21:43:16 +0900

    Feature : Swagger Documentation Applying

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Noonchi API')
+    .setDescription(
+      'Voice Phishing Detecotr Noonchi API Server' +
+        'Token Generateor and Login Process, Data Processing api are included',
+    )
+    .setVersion('v0.1')
+    .addCookieAuth('connect.sid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
